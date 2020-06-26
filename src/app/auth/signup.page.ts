3e92032b38615d67206569ee685338134f4bd71f
unknown <bsse0803@iit.du.ac.bd>
2020-06-27 05:12:37 +0600

    verify national ID

--- src/app/auth/signup.page.ts
+++ src/app/auth/signup.page.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+function verify(){
+  var nidV = document.getElementById('nidV');
+  nidV.onclick = function() {
+    nidV.innerHTML = "Verifying..."
+    nidV.setAttribute("disabled", "disabled");
+    document.getElementById('spinner').style.visibility = "visible";
+    setTimeout(() => {
+      document.getElementById('spinner').style.visibility = "hidden";
+      document.getElementById('success-checkmark').style.display = "block";
+      document.getElementById('check-icon').style.display = "block";
+      nidV.innerHTML = "Verified !";
+      nidV.style.background = "#fff";
+      nidV.style.border = "3px solid #4CAF50"
+      nidV.style.color = "#4CAF50";
+    }, 3000);
+  }
+}
+
 function callImageLoader() {
   var uploader = document.createElement('input'),
     image = document.getElementById('img-result');
@@ -43,6 +61,7 @@ export class SignUpPage implements OnInit {
 
   ngOnInit() {
     callImageLoader();
+    verify()
   }
 
 }
