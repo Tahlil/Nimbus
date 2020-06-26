891fcbfaaa873d2f198a82ec61efabc6f9b55dc0
unknown <bsse0803@iit.du.ac.bd>
2020-06-27 03:30:04 +0600

    added image loader

--- src/app/auth/signup.page.ts
+++ src/app/auth/signup.page.ts
@@ -1,5 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+function callImageLoader() {
+  var uploader = document.createElement('input'),
+    image = document.getElementById('img-result');
+
+  uploader.type = 'file';
+  uploader.accept = 'image/*';
+
+  image.onclick = function() {
+    uploader.click();
+  }
+
+  uploader.onchange = function() {
+    var reader = new FileReader();
+    reader.onload = function(evt) {
+      image.classList.remove('no-image');
+      image.style.backgroundImage = 'url(' + evt.target.result + ')';
+      var request = {
+        itemtype: 'test 1',
+        brand: 'test 2',
+        images: [{
+          data: evt.target.result
+        }]
+      };
+      
+     
+      document.querySelector('.upload-result__content').innerHTML = JSON.stringify(request, null, '  ');
+    }
+    reader.readAsDataURL(uploader.files[0]);
+  }
+};
+
 @Component({
   selector: 'app-auth',
   templateUrl: './signup.page.html',
@@ -7,9 +38,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+    callImageLoader();
   }
 
 }
