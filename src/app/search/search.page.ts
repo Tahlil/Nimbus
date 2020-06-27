e647d2727fa174d5d28d55c487e61857b5729d63
unknown <bsse0803@iit.du.ac.bd>
2020-06-27 09:06:03 +0600

    filter search by price

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -8,12 +8,15 @@ import { ModalPage } from '../modals/modal/modal.page';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+ 
   allInfoGiven:Boolean = false;
   searchInputGiven:Boolean = false;
   selectedDiv:string;
   selectedDis:string;
   selectedSubDis:string;
   selectedMouza:string;
+  max:Number=100000;
+  min:Number=10000;
   constructor(private landInfoService:LandInfoService, public modalController: ModalController) { }
   landData;
   selectedData= [];
@@ -48,15 +51,21 @@ export class SearchPage implements OnInit {
     
   }
 
+  getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
   clickFind(){
     this.selectedData = []
     // if (this.selectedDiv !== "" && this.selectedDis !== "" && this.selectedSubDis !== "" && this.selectedMouza !== ""){
-      console.log(this.landData);
+      
       let lData:any
       for(lData of this.landData){
         if(this.selectedMouza.split('-')[1] === lData.Title_Deed.Area){
           console.log(lData.Title_Deed.Area);
-          this.selectedData.push(lData)
+          lData["minPrice"] = this.getRandomArbitrary(10000, 100000);
+          lData["maxPrice"] = lData["minPrice"] + this.getRandomArbitrary(10000, 100000);
+          this.selectedData.push(lData);
         }
           
       }
@@ -64,7 +73,21 @@ export class SearchPage implements OnInit {
     // }
   }
 
-  async openModal(){
+  filter(){
+    let index = 0;
+    for (const data of this.selectedData) {
+      if(data.minPrice < this.min || data.maxPrice > this.max){
+        this.selectedData.splice(index, 1);
+      }
+      index++;
+    }
+  }
+
+  async openModal($event){
+    $event.target.innerHTML = "Purchase Request Sent";
+    $event.target.setAttribute("disabled", "disabled");
+    $event.target.style.border = "3px solid #4CAF50"
+    $event.target.style.color = "#4CAF50";
     const modal = await this.modalController.create({
       component: ModalPage,
     });
