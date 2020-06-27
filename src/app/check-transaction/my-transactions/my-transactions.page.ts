237a6f7a54de5872980439011ce2661158fa59c2
unknown <bsse0803@iit.du.ac.bd>
2020-06-27 12:53:50 +0600

    My title deed table

--- src/app/check-transaction/my-transactions/my-transactions.page.ts
+++ src/app/check-transaction/my-transactions/my-transactions.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { LandInfoService } from "../../land-info.service";
 @Component({
-  selector: 'app-my-transactions',
-  templateUrl: './my-transactions.page.html',
-  styleUrls: ['./my-transactions.page.scss'],
+  selector: "app-my-transactions",
+  templateUrl: "./my-transactions.page.html",
+  styleUrls: ["./my-transactions.page.scss"],
 })
 export class MyTransactionsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  landData = [];
+  dataLoaded: Boolean = false;
+  constructor(private landInfoService: LandInfoService) {
+    this.landData = [];
+    let that = this;
+    this.dataLoaded = false;
+    this.landInfoService.getJSON().subscribe((datas) => {
+      let cur = 0;
+      for (const data of datas) {
+        cur++;
+        console.log(data.Title_Deed);
+        if (Math.random() > 0.6) {
+          data["mortgage"] = true;
+        } else {
+          data["mortgage"] = false;
+        }
+        if(data)
+          that.landData.push(data);
+        if (cur > 5) {
+          break;
+        }
+        
+      }
+      that.dataLoaded = true;
+    });
   }
 
+  ngOnInit() {}
+
+  checkMortage(hasMortgage) {
+    if(hasMortgage)
+      return hasMortgage.hasOwnProperty('mortgage') ? hasMortgage.mortgage : false;
+    return false;
+  }
+
+  mortgage(){
+
+  }
 }
