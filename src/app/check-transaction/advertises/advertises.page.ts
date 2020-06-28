711dcd16994ceb6b42b7ab5a38b864050042428b
unknown <bsse0803@iit.du.ac.bd>
2020-06-28 10:35:16 +0600

    advertise page

--- src/app/check-transaction/advertises/advertises.page.ts
+++ src/app/check-transaction/advertises/advertises.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LandInfoService } from "../../land-info.service";
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-advertises',
   templateUrl: './advertises.page.html',
   styleUrls: ['./advertises.page.scss'],
 })
 export class AdvertisesPage implements OnInit {
-
-  constructor() { }
+  advertises:any = [];
+  constructor(private landInfoService: LandInfoService, public modalController: ModalController) { }
 
   ngOnInit() {
+    let that = this;
+    this.landInfoService.getJSON().subscribe((datas) => {
+      let cur = 0;
+      
+      for (const data of datas) {
+        cur++;         
+          if(data && cur > 111 && cur<123)
+            that.advertises.push(data);
+          if (cur > 124) {
+            break;
+          }
+        }
+
+    });
   }
 
 }
